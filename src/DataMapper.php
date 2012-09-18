eb81db1a6bcf0c98e064512ea367018364a156d9
Alessandro Perta <alessandro.perta@gmail.com>
2012-09-19 00:19:34 +0200

    added doc

--- src/DataMapper.php
+++ src/DataMapper.php
@@ -2,15 +2,39 @@
 
 namespace RedefineLab\DataMapper;
 
+/**
+ * RedefineLab DataMapper
+ * 
+ * RedefineLab DataMapper is a simple library whose aim is to easily map objects
+ * to arrays and arrays to objects. Useful for fast database insertion from POPO
+ * and getting POPO from array fetched from database.
+ *
+ * @author Alessandro Perta <alessandro.perta@gmail.com>
+ */
 class DataMapper
 {
 
+    public function __construct()
+    {
+        ;
+    }
+
+    /**
+     * @param array $array An array to be converted in an object
+     * @param string $className The class of the returned object
+     * @return object An object populated by the array
+     */
     public function arrayToObject($array, $className)
     {
         $object = new $className;
         return $this->bulkSet($array, $object);
     }
 
+    /**
+     * @param object $object The object to be converted in an array
+     * @param bool $removeEmptyKeys Whether to include empty properties in the final array
+     * @return array The array created from the object
+     */
     public function objectToArray($object, $removeEmptyKeys = false)
     {
         $array = array();
@@ -32,6 +56,11 @@ class DataMapper
         return $array;
     }
 
+    /**
+     * @param array $properties An array of properties to set on the object
+     * @param object $object The object to populate from the array
+     * @return object The populated object
+     */
     public function bulkSet(array $properties, $object)
     {
         foreach ($properties as $property => $value)
