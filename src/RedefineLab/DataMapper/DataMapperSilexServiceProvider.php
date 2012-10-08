754069f51d85afd4600c3d0f0d012b23a9eb400c
Alessandro Perta <alessandro.perta@gmail.com>
2012-10-08 23:59:30 +0300

    swapped boot and register methods

--- src/RedefineLab/DataMapper/DataMapperSilexServiceProvider.php
+++ src/RedefineLab/DataMapper/DataMapperSilexServiceProvider.php
@@ -7,14 +7,14 @@ class DataMapperSilexServiceProvider implements \Silex\ServiceProviderInterface
 
     public function boot(\Silex\Application $app)
     {
-        $app['data_mapper'] = $app->share(function () use ($app) {
-            return new DataMapper();
-        });
+        // Nothing to do here
     }
 
     public function register(\Silex\Application $app)
     {
-        // Nothing to do here
+        $app['data_mapper'] = $app->share(function () use ($app) {
+            return new DataMapper();
+        });
     }
 
 }
